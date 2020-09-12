2a434a6c21352ae698a67fa37023f5b9d30201ad
Knoxik <iknoxik@yandex.ru>
2020-09-12 21:22:09 +0300

    setup some comp.

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -20,14 +20,25 @@ export default class App extends Component {
     super(props);
     this.state = {
       data: [
-        { label: "Going to learn React", important: true, id: "1" },
-        { label: "That is so good", important: true, id: "2" },
-        { label: "I need a break...", important: false, id: "3" },
+        {
+          label: "Going to learn React",
+          important: true,
+          id: "1",
+          like: false,
+        },
+        { label: "That is so good", important: true, id: "2", like: false },
+        { label: "I need a break...", important: false, id: "3", like: false },
       ],
+      term: "",
+      filter: "all",
     };
 
     this.deletItem = this.deletItem.bind(this);
     this.addItem = this.addItem.bind(this);
+    this.onToggleImportant = this.onToggleImportant.bind(this);
+    this.onToggleLiked = this.onToggleLiked.bind(this);
+    this.onUpDateSearch = this.onUpDateSearch.bind(this);
+    this.onFilterSelect = this.onFilterSelect.bind(this);
 
     this.maxId = 4;
   }
@@ -36,10 +47,7 @@ export default class App extends Component {
     this.setState(({ data }) => {
       const index = data.findIndex((el) => el.id === id);
 
-      const before = data.slice(0, index);
-      const after = data.slice(index + 1);
-
-      const newArr = [...before, ...after];
+      const newArr = [...data.slice(0, index), ...data.slice(index + 1)];
 
       return {
         data: newArr,
@@ -62,15 +70,94 @@ export default class App extends Component {
     });
   }
 
+  onToggleImportant(id) {
+    this.setState(({ data }) => {
+      const index = data.findIndex((el) => el.id === id);
+
+      const old = data[index];
+      const newItem = { ...old, important: !old.important };
+
+      const newArr = [
+        ...data.slice(0, index),
+        newItem,
+        ...data.slice(index + 1),
+      ];
+
+      return {
+        data: newArr,
+      };
+    });
+  }
+
+  onToggleLiked(id) {
+    this.setState(({ data }) => {
+      const index = data.findIndex((el) => el.id === id);
+
+      const old = data[index];
+      const newItem = { ...old, like: !old.like };
+
+      const newArr = [
+        ...data.slice(0, index),
+        newItem,
+        ...data.slice(index + 1),
+      ];
+
+      return {
+        data: newArr,
+      };
+    });
+  }
+
+  searchPost(items, term) {
+    if (term.length === 0) {
+      return items;
+    }
+
+    return items.filter((item) => {
+      return item.label.indexOf(term) > -1;
+    });
+  }
+
+  filterPost(items, filter) {
+    if (filter === "like") {
+      return items.filter((item) => item.like);
+    } else {
+      return items;
+    }
+  }
+
+  onUpDateSearch(term) {
+    this.setState({ term });
+  }
+
+  onFilterSelect(filter) {
+    this.setState({ filter });
+  }
+
   render() {
+    const { data, term, filter } = this.state;
+
+    const liked = data.filter((item) => item.like).length;
+    const allPosts = data.length;
+
+    const visiblePosts = this.filterPost(this.searchPost(data, term), filter);
+
     return (
       <AppBlock>
-        <AppHeader />
+        <AppHeader liked={liked} allPosts={allPosts} />
         <div className="search-panel d-flex">
-          <SearchPanel />
-          <PostStatusFilter />
+          <SearchPanel onUpDateSearch={this.onUpDateSearch} />
+          <PostStatusFilter
+            filter={filter}
+            onFilterSelect={this.onFilterSelect}
+          />
         </div>
-        <PostList posts={this.state.data} onDelete={this.deletItem} />
+        <PostList
+          posts={visiblePosts}
+          onDelete={this.deletItem}
+          onToggleImportant={this.onToggleImportant}
+          onToggleLiked={this.onToggleLiked}
+        />
         <PostAddForm onAdd={this.addItem} />
       </AppBlock>
     );
--- src/components/post-status-filter/post-status-filter.js
+++ src/components/post-status-filter/post-status-filter.js
@@ -1,20 +1,34 @@
-import React from "react";
-
-import { Button } from "reactstrap";
+import React, { Component } from "react";
 
 import "./post-status-filter.css";
 
-const PostStatusFilter = () => {
-  return (
-    <div className="btn-group">
-      <Button outline color="info">
-        Все
-      </Button>
-      <button type="button" className="btn btn-outline-secondary">
-        Понравилось
-      </button>
-    </div>
-  );
-};
+export default class PostStatusFilter extends Component {
+  constructor(props) {
+    super(props);
 
-export default PostStatusFilter;
+    this.buttons = [
+      { name: "all", label: "Все" },
+      { name: "like", label: "Понравилось" },
+    ];
+  }
+
+  render() {
+    const buttons = this.buttons.map(({ name, label }) => {
+      const { filter, onFilterSelect } = this.props;
+      const active = filter === name;
+      const clazz = active ? "btn-info" : "btn-outline-secondaty";
+      return (
+        <button
+          key={name}
+          type="button"
+          className={`btn ${clazz}`}
+          onClick={() => onFilterSelect(name)}
+        >
+          {label}
+        </button>
+      );
+    });
+
+    return <div className="btn-group">{buttons}</div>;
+  }
+}
